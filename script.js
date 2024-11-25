ff8d3749b302bf0f79b3c9ee7a081d65cf026e8c
AshFlav <ashimmalbar@gmail.com>
2024-11-25 08:55:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Toggle class active
-const navbarNav = document.querySelector ('.navbar-nav');
-// ketika hamburger menu diklik
-document.querySelector('#hamburger-menu').onclick = () => {
-navbarNav.classList.toggle('active');
-};
+const navbarNav = document.querySelector('.navbar-nav');
+// When hamburger menu is clicked
+const hamburgerMenu = document.querySelector('#hamburger-menu');
 
-// Klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector ('#hamburger-menu');
+hamburgerMenu.addEventListener('click', () => {
+  navbarNav.classList.toggle('active');
+});
 
-document.addEventListener('click', function(e){
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
-    }
+// Click outside the sidebar to remove the nav
+document.addEventListener('click', (e) => {
+  if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
 });
